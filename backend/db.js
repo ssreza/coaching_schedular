fddef2ba6f3770b14cf6cf06bfe093d4a5243d4a
reza <shifatsalim@gmail.com>
2024-01-16 12:23:13 -0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,7 @@
 const { Client, Pool } = require('pg')
 // const pgp = require('pg-promise')
 const connectionString = "postgres://ekpsddoc:CjWTzXqFygiBVETbJApNL0YYLvhOvOJc@heffalump.db.elephantsql.com/ekpsddoc"
-// const pool = new Pool({
-//     user: 'ekpsddoc',
-//     host: 'heffalump.db.elephantsql.com',
-//     database: 'coaching_schedular',
-//     password: 'CjWTzXqFygiBVETbJApNL0YYLvhOvOJc ',
-//     port: 5432,
-//      ssl: false, // Make sure to set this to true if your RDS instance requires SSL
-// });
+
 const knex = require('knex')({
   client: 'pg',
   connection: connectionString,
@@ -17,14 +10,7 @@ const knex = require('knex')({
 const pool = new Pool({
     connectionString,
   })
-// const cn = {
-//     user: 'ekpsddoc',
-//     host: 'heffalump.db.elephantsql.com',
-//     database: 'coaching_schedular',
-//     password: 'CjWTzXqFygiBVETbJApNL0YYLvhOvOJc ',
-//     port: 5432,
-// };
-// const database = pgp("postgres://ekpsddoc:CjWTzXqFygiBVETbJApNL0YYLvhOvOJc@heffalump.db.elephantsql.com/ekpsddoc");
+
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
